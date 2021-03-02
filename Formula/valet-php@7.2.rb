8e87b609188ceefed4379b3eded4eed067a24324
Mikel Martin <mikel@zhenit.com>
2021-03-02 18:10:05 +0100

    Fix for issue #134
    https://github.com/henkrehorst/homebrew-php/issues/134

--- Formula/valet-php@7.2.rb
+++ Formula/valet-php@7.2.rb
@@ -52,7 +52,9 @@ class ValetPhpAT72 < Formula
     # Work around configure issues with Xcode 12
     # See https://bugs.php.net/bug.php?id=80171
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
-
+    # Workaround for https://bugs.php.net/80310
+    ENV.append "CFLAGS", "-DU_DEFINE_FALSE_AND_TRUE=1"
+    ENV.append "CXXFLAGS", "-DU_DEFINE_FALSE_AND_TRUE=1"
     # buildconf required due to system library linking bug patch
     system "./buildconf", "--force"
 
